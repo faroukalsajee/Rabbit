b4123db69282b6b9e4d8f69b91dc58110cf1364b
faroukalsajee <faroukalsajee@gmail.com>
2021-06-30 23:17:26 -0700

    build chakra components

---
+++ Web/src/pages/register.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Formik, Form } from 'formik'
+interface registerProps { }
+
+
+
+const register: React.FC<registerProps> = ({ }) => {
+  return (
+    <Formik initialValues={{ username: "", password: "" }}
+      onSubmit={(values) => {
+        console.log(values);
+
+      }}
+    >
+      {() => (
+        <Form>
+
+          <div>hello</div>
+
+        </Form>
+
+      )}
+
+    </Formik>
+
+  );
+}
+
+export default register
