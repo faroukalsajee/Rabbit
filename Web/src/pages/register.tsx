091a4d6957448b6a979dcc34890e65d1748a38e0
faroukalsajee <faroukalsajee@gmail.com>
2021-07-01 12:43:39 -0700

    finish wrapper and input fields components

--- Web/src/pages/register.tsx
+++ Web/src/pages/register.tsx
@@ -1,29 +1,55 @@
-import React from 'react'
-import { Formik, Form } from 'formik'
+import React from "react";
+import { Formik, Form } from "formik";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Box,
+  Button,
+} from "@chakra-ui/core";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+
 interface registerProps { }
 
-
-
-const register: React.FC<registerProps> = ({ }) => {
+const Register: React.FC<registerProps> = ({ }) => {
   return (
-    <Formik initialValues={{ username: "", password: "" }}
-      onSubmit={(values) => {
-        console.log(values);
-
-      }}
-    >
-      {() => (
-        <Form>
-
-          <div>hello</div>
-
-        </Form>
-
-      )}
-
-    </Formik>
-
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              variantColor="teal"
+            >
+              register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
   );
-}
+};
 
-export default register
+export default Register;
