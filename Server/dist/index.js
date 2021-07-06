9a1a8de00e8725288f2514695d550cecc4bd48cd
faroukalsajee <faroukalsajee@gmail.com>
2021-07-06 16:34:48 -0700

    setup urql and generate graphql query

--- Server/dist/index.js
+++ Server/dist/index.js
@@ -25,14 +25,19 @@ const user_1 = require("./resolvers/user");
 const redis_1 = __importDefault(require("redis"));
 const express_session_1 = __importDefault(require("express-session"));
 const connect_redis_1 = __importDefault(require("connect-redis"));
+const cors_1 = __importDefault(require("cors"));
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
     yield orm.getMigrator().up();
     const app = express_1.default();
     const RedisStore = connect_redis_1.default(express_session_1.default);
     const redisClient = redis_1.default.createClient();
+    app.use(cors_1.default({
+        origin: "http://localhost:3000",
+        credentials: true,
+    }));
     app.use(express_session_1.default({
-        name: 'qid',
+        name: "qid",
         store: new RedisStore({
             client: redisClient,
             disableTouch: true,
@@ -44,7 +49,7 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
             secure: constants_1.__prod__,
         },
         saveUninitialized: false,
-        secret: "ergqerhqerjqerjq",
+        secret: "qowiueojwojfalksdjoqiwueo",
         resave: false,
     }));
     const apolloServer = new apollo_server_express_1.ApolloServer({
@@ -54,9 +59,12 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
         }),
         context: ({ req, res }) => ({ em: orm.em, req, res }),
     });
-    apolloServer.applyMiddleware({ app });
+    apolloServer.applyMiddleware({
+        app,
+        cors: false,
+    });
     app.listen(4000, () => {
-        console.log('server started on port 4000');
+        console.log("server started on localhost:4000");
     });
 });
 main().catch((err) => {
--- Web/src/pages/_app.tsx
+++ Web/src/pages/_app.tsx
@@ -1,17 +1,28 @@
 import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/react'
+import { Provider, createClient } from "urql";
+
+const client = createClient({
+  url: "http://localhost:4000/graphql",
+  fetchOptions: {
+    credentials: "include",
+  },
+});
 
 import theme from '../theme'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <ColorModeProvider options={{
-        useSystemColorMode: true
-      }}>
-        <CSSReset />
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ThemeProvider>
+    <Provider value={client}>
+      <ThemeProvider theme={theme}>
+        <ColorModeProvider options={{
+          useSystemColorMode: true
+        }}>
+          <CSSReset />
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ThemeProvider>
+    </Provider>
+
   )
 }
 
